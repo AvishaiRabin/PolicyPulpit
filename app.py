2ef455384811acac94649a53bd9792cf9b3b2b04
Avi Student <avirabin@berkeley.edu>
2018-09-08 18:12:17 -0700

    Somehow lost the html code, starting that part from scratch now

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 pd.set_option('max_colwidth', 280)
 from tweets import *
 import os
+from bokeh.embed import components
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -14,13 +15,39 @@ def index():
         visualize('download')
         button = request.form['submit']
         if button == 'download':
+            namestable = pd.read_csv('names.csv')
             with open(key_file) as f:
                 session['keys'] = json.load(f)
-            download_tweets("realdonaldtrump")
-            return render_template('index.html')
-        elif button == 'visualize':
-            visualize('visualize')
-            load_tweets("realdonaldtrump")
+            download_all_tweets(namestable)
+            return redirect('/')
+        elif button == 'visualize tweet source':
+            visualize('visualizetweetsource')
+            namestable = pd.read_csv('names.csv')
+            scounts = sourcecounts(namestable)
+            script, div = components(scounts)
+            return render_template('index.html', script=script, div=div)
+        elif button == 'visualize number of tweets over time':
+            visualize('visualizetweettime')
+            namestable = pd.read_csv('names.csv')
+            times = tweettimes(namestable)
+            script, div = components(times)
+            return render_template('index.html', script=script, div=div)
+        elif button == 'visualize most retweeted tweets':
+            visualize('visualizeretweets')
+            namestable = pd.read_csv('names.csv')
+            retweets = retweetcounts(namestable).to_html(index=False, escape=True)
+            return render_template('index.html', tables=[retweets], titles=['na', 'Most Retweeted Tweets'])
+        elif button == 'visualize tweet sentiment analysis':
+            visualize('visualizesentiments')
+            namestable = pd.read_csv('names.csv')
+            sentiment = pd.read_table('vader_lexicon.txt')
+            sentitweets = sentanalysis(namestable, sentiment).to_html(index=False, escape=True)
+            return render_template('index.html', tables=[sentitweets], titles=['na', 'Sentiment Analysis'])
+        elif button == 'update senate list':
+            names = get_handles()
+            pd.DataFrame(names, columns=["Senator", "Handle"]).to_csv('names.csv', index=False)
+            session['handles'] = names
+            visualize('names')
             return redirect('/')
     return render_template('index.html')
 
--- tweets.py
+++ tweets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 pd.set_option('max_colwidth', 280)
 import tweepy
 import json
@@ -8,9 +9,36 @@ from pathlib import Path
 from tweepy import TweepError
 import logging
 import datetime
+import selenium
+import re
+from bokeh.plotting import figure
+from dateutil import parser
+
+def get_handles():
+    html = "https://www.socialseer.com/resources/us-senator-twitter-accounts/"
+    from selenium import webdriver
+    senate = webdriver.Firefox()
+    senate.get(html)
+    names = []
+    senaterange = range(2, 102)
+    for i in senaterange:
+        path1 = "/html/body/div/div/div/div/article/div/table[1]/tbody/tr[%s]/td[3]/a" % (i)
+        element1 = senate.find_element_by_xpath(path1)
+        name1 = element1.text
+        path2 = "/html/body/div/div/div/div/article/div/table[1]/tbody/tr[%s]/td[2]" % (i)
+        element2 = senate.find_element_by_xpath(path2)
+        name2 = element2.text
+        names.append([name2, name1])
+    return names
+
+def download_all_tweets(namestable):
+    names = namestable['Handle']
+    for name in names:
+        download_tweets(name)
 
 # Downloads all tweets in the last week from a user and stores it in a json file under the name of their username
 def download_tweets(username):
+    print(username)
     today = datetime.date.today()
     today = datetime.datetime(today.year, today.month, today.day)
     week_ago = today - datetime.timedelta(days=7)
@@ -37,13 +65,107 @@ def download_tweets(username):
         json.dump(new_tweets, f)
     session[username] = save_path
 
+def sourcecounts(namestable):
+    handles = namestable['Handle']
+    tweets = []
+    for i in handles:
+        tweets.append(load_tweets(i))
+    fulltweets = []
+    for tweetlist in tweets:
+        for tweet in tweetlist:
+            fulltweets.append(re.findall('>.+?<', tweet['source'])[0][1:-1])
+    fulltweets = pd.Series(fulltweets).value_counts().to_frame()
+    fulltweets = fulltweets.rename(columns={0:'value counts'})
+    categories =  [i for i in fulltweets.index]
+    colors = ['red', 'orange', 'gold', 'green', 'blue', 'indigo', 'violet']
+    p = figure(x_range=categories, plot_height=400, plot_width=1250, title="Tweet Source Counts",
+    toolbar_location='above')
+    p.vbar(x=categories, top=[i for i in fulltweets['value counts']], width=0.9, color=colors)
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+    return p
+
+def tweettimes(namestable):
+    handles = namestable['Handle']
+    tweets = []
+    for i in handles:
+        tweets.append(load_tweets(i))
+    fulltweets = []
+    for tweetlist in tweets:
+        for tweet in tweetlist:
+            fulltweets.append(parser.parse(tweet['created_at']).replace(minute=0, second=0))
+    fulltweets = pd.Series(fulltweets).value_counts().to_frame()
+    fulltweets = fulltweets.rename(columns={0:'Time of Tweet'})
+    fulltweets = fulltweets.sort_index()
+    categories = [i for i in fulltweets.index]
+    p = figure(
+        plot_height=400, plot_width=1250, title="Tweet activity of all U.S. senators in the last week",
+        toolbar_location='above', x_axis_type='datetime'
+        )
+    p.line(x=categories, y=[i for i in fulltweets['Time of Tweet']], line_width=0.9)
+    p.xgrid.grid_line_color=None
+    return p
+
+def retweetcounts(namestable):
+    handles = namestable['Handle']
+    tweets = []
+    for i in handles:
+        tweets.append(load_tweets(i))
+    fulltweets = []
+    for tweetlist in tweets:
+        for tweet in tweetlist:
+            fulltweets.append([tweet['user']['name'], tweet['full_text'], tweet['retweet_count']])
+    fulltweets = pd.DataFrame(fulltweets, columns=['Name', 'Text', 'Retweet Count'])
+    fulltweets = fulltweets.sort_values(by=['Retweet Count'])
+    return fulltweets.tail(10).iloc[::-1]
+
+def sentanalysis(namestable, lexicon):
+    lexicon.loc[-1] = lexicon.columns
+    lexicon.index = lexicon.index+1
+    lexicon = lexicon.sort_index()
+    lexicon.index = lexicon['$:']
+    lexicon = lexicon.rename(columns = {'-1.5' : 'polarity'})
+    lexicon = lexicon[['polarity']]
+    lexicon.index = lexicon.index.rename('token')
+    lexicon['polarity'] = [float(i) for i in lexicon['polarity']]
+    handles = namestable['Handle']
+    tweets = []
+    for i in handles:
+        tweets.append(load_tweets(i))
+    fulltweets = []
+    for tweetlist in tweets:
+        for tweet in tweetlist:
+            fulltweets.append([tweet['user']['name'], tweet['full_text']])
+    fulltweets = pd.DataFrame(fulltweets, columns=['Name', 'Text'])
+    fulltweets['Text'] = fulltweets['Text'].str.lower().str.replace('[^\s\w]', ' ')
+    fulltweets['Text'] = [eval(i, lexicon) for i in fulltweets['Text']]
+    toptweets = most_positive_tweets(fulltweets)
+    categories = [i for i in toptweets.index]
+    p = figure(x_range=categories, plot_height=400, plot_width=1250, title="Tweet Source Counts",
+    toolbar_location='above')
+    p.vbar(x=categories, top=[i for i in fulltweets['Text']], width=0.9)
+    p.xgrid.grid_line_color = None
+    p.y_range.start = 0
+    return p
+
+
+def most_positive_tweets(table):
+    return table.groupby('Name').mean().sort_values('Text').iloc[::-1]
+
+
 def load_tweets(username):
     save_path = username + '.json'
     with open(save_path, "r") as f:
-        session[username] = json.load(f)
+        return json.load(f)
 
 def visualize(keyword):
-    indicators_to_show = ['visualize', 'download']
+    indicators_to_show = ['visualizetweetsource', 'download', 'names', 'visualizetweettime', 'visualizeretweets']
     for i in indicators_to_show:
         session[i] = False
     session[keyword] = True
+
+def eval(tweet, lexicon):
+    tweet = tweet.split()
+    tweet = pd.DataFrame(tweet, columns=['text'])
+    tweet = tweet.merge(lexicon, left_on='text', right_index=True, how='left').fillna(0)
+    return np.sum(tweet['polarity'])
